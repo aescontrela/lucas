d6fa5ddec20d326496b3cce8f2c325426867f935
Ana Escontrela <anaescontrela@gmail.com>
2026-04-12 19:02:40 +0100

    refactor research section

--- frontend/src/router.tsx
+++
@@ -1,4 +0,0 @@
-import { createBrowserRouter } from "react-router";
-import { Search } from "./pages/search/page";
-
-export const router = createBrowserRouter([{ path: "/", element: <Search /> }]);
